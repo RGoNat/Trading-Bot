0f456511beeb2ad4f80ccf8bf105f53912a5e242
RGoNat <shockwavex001@gmail.com>
2019-06-01 22:29:48 -0700

    Start New

---
+++ strategies/DeepRL.js
@@ -0,0 +1,27 @@
+var strat = {}
+
+console.log('hello world')
+
+strat.init = function(){
+
+}
+
+strat.update = function(cancle){
+
+}
+
+strat.log = function(){
+
+}
+
+strat.check = function(){
+
+    this.advice('long')
+}
+
+strat.end = function(){
+
+}
+
+module.exports = strat;
+
